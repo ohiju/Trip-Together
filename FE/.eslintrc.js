4fd650e71169340b37e2c380f14411e245e8daec
KTG <xorb269@naver.com>
2024-03-12 09:25:44 +0900

    S10P22A309-188 docs: styled-component 설정 완료

---
+++ FE/.eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  root: true,
+  extends: '@react-native',
+  rules: {
+    'prettier/perttier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
+  },
+};
---
+++ FE/src/App.tsx
@@ -0,0 +1,20 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {Text} from 'react-native';
+import {StyledView} from './AppStyle';
+
+function App(): React.JSX.Element {
+  return (
+    <StyledView>
+      <Text>hi</Text>
+    </StyledView>
+  );
+}
+
+export default App;
