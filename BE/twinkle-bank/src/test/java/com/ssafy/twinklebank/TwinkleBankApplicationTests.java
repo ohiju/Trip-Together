4441caf5a91eeaea48d5d29255cea2eb1aef26d8
sirlyun <sirlyun23@gmail.com>
2024-03-13 10:18:53 +0900

    Feat: Back-end 폴더 Init

---
+++ BE/twinkle-bank/src/test/java/com/ssafy/twinklebank/TwinkleBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.twinklebank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwinkleBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
