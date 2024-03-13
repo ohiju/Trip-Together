b7fec306ae888e465e70e84e1d5673dcde3c3072
ohiju <ohiju@naver.com>
2024-03-13 16:41:00 +0900

    S10P22A309-195 Feat: 여행지 추천 캐러셀

--- FE/src/components/Travel/Package.tsx
+++ FE/src/components/Travel/Package.tsx
@@ -1,12 +1,49 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {FlatList} from 'react-native';
+import styled from 'styled-components/native';
+import Page from '../../assets/data/Page';
 
-function Package() {
-  return (
-    <View>
-      <Text>로그인</Text>
-    </View>
-  );
+interface ICarousel {
+  gap: number;
+  offset: number;
+  pages: any[];
+  pageWidth: number;
 }
 
-export default Package;
+const Container = styled.View`
+  height: 40%;
+  width: 90%;
+  padding: 40px 0px;
+  background-color: white;
+  justify-content: center;
+  align-items: center;
+`;
+
+export default function Carousel({pages, pageWidth, gap, offset}: ICarousel) {
+  function renderItem({item}: any) {
+    return (
+      <Page item={item} style={{width: pageWidth, marginHorizontal: gap / 2}} />
+    );
+  }
+
+  return (
+    <Container>
+      {/* <Text>여행 상품 추천</Text> */}
+      <FlatList
+        automaticallyAdjustContentInsets={false}
+        contentContainerStyle={{
+          paddingHorizontal: (offset + gap / 2) / 4,
+        }}
+        data={pages}
+        decelerationRate="fast"
+        horizontal
+        keyExtractor={(item: any) => `page__${item.color}`}
+        pagingEnabled
+        renderItem={renderItem}
+        snapToInterval={pageWidth + gap}
+        snapToAlignment="start"
+        showsHorizontalScrollIndicator={false}
+      />
+    </Container>
+  );
+}
--- FE/src/pages/travel.tsx
+++ FE/src/pages/travel.tsx
@@ -1,18 +1,47 @@
 import React from 'react';
-import {Text} from 'react-native';
-import {Wrapper, RecommendView, IconView} from './TravelStyle';
+import {Text, Dimensions} from 'react-native';
+import {Wrapper, IconView} from './TravelStyle';
 import Plans from '../components/Travel/Plans';
+import Package from '../components/Travel/Package';
 
 function Travel() {
+  const screenWidth = Math.round(Dimensions.get('window').width);
+
+  const PAGES = [
+    {
+      num: 1,
+      color: '#86E3CE',
+    },
+    {
+      num: 2,
+      color: '#D0E6A5',
+    },
+    {
+      num: 3,
+      color: '#FFDD94',
+    },
+    {
+      num: 4,
+      color: '#FA897B',
+    },
+    {
+      num: 5,
+      color: '#CCABD8',
+    },
+  ];
+
   return (
     <Wrapper>
       <Plans />
       <IconView>
         <Text>1</Text>
       </IconView>
-      <RecommendView>
-        <Text>1</Text>
-      </RecommendView>
+      <Package
+        gap={15}
+        offset={35}
+        pages={PAGES}
+        pageWidth={screenWidth - (16 + 36) * 5}
+      />
     </Wrapper>
   );
 }
