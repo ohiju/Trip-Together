2fcb41f2c75e4a02c27ef4b0cef51e3a910e51ed
황진하 <hjh70902000@naver.com>
2024-03-18 18:02:04 +0900

    S10P22A309-87 Feat: 전역 에러 처리 추가

---
+++ BE/trip-together/src/main/java/com/ssafy/triptogether/global/exception/exceptions/category/ConflictException.java
@@ -0,0 +1,14 @@
+package com.ssafy.triptogether.global.exception.exceptions.category;
+
+import com.ssafy.triptogether.global.exception.response.ErrorCode;
+
+/**
+ * 409
+ * 리소스 등의 충돌
+ */
+public class ConflictException extends TripRuntimeException {
+	protected static final String MESSAGE_KEY = "error.Conflict";
+	public ConflictException(String detailMessageKey, ErrorCode errorCode, Object[] params) {
+		super(MESSAGE_KEY + "." + detailMessageKey, errorCode, params);
+	}
+}
