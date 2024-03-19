e05cbee3c645711b904c03896dffe453c48d5aaa
seomiii <seomiii1765@gmail.com>
2024-03-19 16:38:59 +0900

    S10P22A309-166 Feat: 은행 로그인 api 구현

--- BE/twinkle-bank/src/main/java/com/ssafy/twinklebank/member/repository/MemberRepository.java
+++ BE/twinkle-bank/src/main/java/com/ssafy/twinklebank/member/repository/MemberRepository.java
@@ -1,8 +1,11 @@
 package com.ssafy.twinklebank.member.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.ssafy.twinklebank.member.domain.Member;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
+	Optional<Member> findMemberByUsername(String username);
 }
