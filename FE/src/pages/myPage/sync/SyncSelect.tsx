3ca74ac8bec7f451376211b86cbf31e3db9b38dd
KTG <xorb269@naver.com>
2024-03-19 09:27:48 +0900

    S10P22A309-108 feat: 계좌 선택 페이지 퍼블리시중

--- FE/src/pages/myPage/sync/SyncSelect.tsx
+++ FE/src/pages/myPage/sync/SyncSelect.tsx
@@ -1,4 +1,8 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {WithLocalSvg} from 'react-native-svg/css';
+import bankAccounts, {bankAccount} from '../../../assets/data/bankAccount';
+import CaretSvg from '../../../assets/icons/caret.svg';
+import {imagePath} from '../../../assets/images/imagePath';
 import AppButton from '../../../components/common/AppButton';
 import {
   Body,
@@ -9,9 +13,28 @@ import {
   TitleView,
 } from '../../../components/common/InfoPageStyle';
 import {BottomButton} from '../../../constants/AppButton';
-import {Wrapper} from './SyncSelectStyle';
+import {
+  Balance,
+  BalanceView,
+  BankLogo,
+  Option,
+  OptionBox,
+  OptionView,
+  Options,
+  Select,
+  SelectBox,
+  SelectView,
+  Wrapper,
+} from './SyncSelectStyle';
 
 const SyncSelect = () => {
+  const [opened, setOpened] = useState<boolean>(false);
+  const [selected, setSelected] = useState<bankAccount | null>(null);
+
+  const localing = (num: number) => {
+    return num.toLocaleString('ko-KR');
+  };
+
   return (
     <Wrapper>
       <TitleView>
@@ -23,8 +46,44 @@ const SyncSelect = () => {
         <Slogan>Trip Together에서 사용할 반짝은행 계좌를</Slogan>
         <Slogan>선택해주세요.</Slogan>
       </SloganView>
-      <Body></Body>
-      <AppButton style={BottomButton} text="다음" />
+      <Body>
+        <SelectBox>
+          <SelectView>
+            {selected ? (
+              <OptionView>
+                <BankLogo source={imagePath.bankLogo} resizeMode="contain" />
+                <Option>{selected.account_num}</Option>
+              </OptionView>
+            ) : (
+              <Select>계좌 선택</Select>
+            )}
+            <WithLocalSvg
+              width={25}
+              height={25}
+              rotation={opened ? 270 : 90}
+              asset={CaretSvg}
+            />
+          </SelectView>
+          <Options>
+            {bankAccounts.map(account => (
+              <OptionBox key={account.account_uuid}>
+                <OptionView>
+                  <BankLogo source={imagePath.bankLogo} resizeMode="contain" />
+                  <Option>{account.account_num}</Option>
+                </OptionView>
+                <BalanceView>
+                  <Balance>잔액 {localing(account.balance)} 원</Balance>
+                </BalanceView>
+              </OptionBox>
+            ))}
+          </Options>
+        </SelectBox>
+      </Body>
+      <AppButton
+        style={BottomButton}
+        text="다음"
+        disabled={selected === null}
+      />
     </Wrapper>
   );
 };
--- FE/src/pages/myPage/sync/SyncSelectStyle.ts
+++ FE/src/pages/myPage/sync/SyncSelectStyle.ts
@@ -6,4 +6,58 @@ const Wrapper = styled.View`
   background: ${bg_light};
 `;
 
-export {Wrapper};
+const SelectBox = styled.View`
+  flex: 1;
+  padding: 15px;
+`;
+
+const SelectView = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  border-bottom-width: 1px;
+  padding: 10px;
+`;
+
+const Select = styled.Text`
+  font-size: 18px;
+`;
+
+const Options = styled.ScrollView`
+  margin: 10px 0;
+`;
+
+const OptionBox = styled.View`
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const OptionView = styled.View`
+  flex-direction: row;
+  align-items: center;
+`;
+
+const BankLogo = styled.Image`
+  width: 40px;
+  height: 40px;
+`;
+
+const Option = styled.Text``;
+
+const BalanceView = styled.View``;
+
+const Balance = styled.Text``;
+
+export {
+  Balance,
+  BalanceView,
+  BankLogo,
+  Option,
+  OptionBox,
+  OptionView,
+  Options,
+  Select,
+  SelectBox,
+  SelectView,
+  Wrapper,
+};
