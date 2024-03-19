9f60f56a7f364b2423c88eb601d3f6db4daf9ef8
hgene0929 <hgene0929@gmail.com>
2024-03-19 09:38:16 +0900

    S10P22A309-167 Test: 프로필 수정 API 서비스 단위 테스트

--- BE/trip-together/src/test/java/com/ssafy/triptogether/member/service/MemberServiceImplTest.java
+++ BE/trip-together/src/test/java/com/ssafy/triptogether/member/service/MemberServiceImplTest.java
@@ -1,5 +1,6 @@
 package com.ssafy.triptogether.member.service;
 
+import com.ssafy.triptogether.global.exception.exceptions.category.NotFoundException;
 import com.ssafy.triptogether.member.data.ProfileUpdateRequest;
 import com.ssafy.triptogether.member.repository.MemberRepository;
 import org.junit.jupiter.api.DisplayName;
@@ -9,7 +10,8 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static com.ssafy.triptogether.global.exception.response.ErrorCode.UNDEFINED_MEMBER;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @ExtendWith(MockitoExtension.class)
@@ -25,13 +27,16 @@ class MemberServiceImplTest {
     void profileUpdateNotFoundException() {
         // given
         long undefinedId = 1L;
-        ProfileUpdateRequest request = new ProfileUpdateRequest(
-                "/S3/profile/1", "멍멍이", "자유로운 영혼"
-        );
+        ProfileUpdateRequest request
+                = new ProfileUpdateRequest("/S3/profile/1", "멍멍이", "자유로운 영혼");
 
-        // when & then
-        RuntimeException exception = assertThrows(
-                RuntimeException.class, () -> memberService.updateProfile(undefinedId, request)
-        );
+        // when
+        NotFoundException exception
+                = assertThrows(NotFoundException.class, () -> memberService.updateProfile(undefinedId, request));
+
+        // then
+        assertThat(exception.getMessageKey()).isEqualTo("error.NotFound.ProfileUpdate");
+        assertThat(exception.getErrorCode()).isEqualTo(UNDEFINED_MEMBER);
+        assertThat(exception.getParams()).contains(undefinedId);
     }
 }
