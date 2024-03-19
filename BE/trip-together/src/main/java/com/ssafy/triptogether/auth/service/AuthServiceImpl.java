be213df275b25ac0bd526216a043a0affaa86650
sirlyun <sirlyun23@gmail.com>
2024-03-19 11:31:07 +0900

    S10P22A309-214 Feat: Member Not Found 에러 처리

--- BE/trip-together/src/main/java/com/ssafy/triptogether/auth/service/AuthServiceImpl.java
+++ BE/trip-together/src/main/java/com/ssafy/triptogether/auth/service/AuthServiceImpl.java
@@ -3,6 +3,9 @@ package com.ssafy.triptogether.auth.service;
 import org.springframework.stereotype.Service;
 
 import com.ssafy.triptogether.auth.data.request.PinVerifyRequest;
+import com.ssafy.triptogether.global.exception.exceptions.category.BadRequestException;
+import com.ssafy.triptogether.global.exception.exceptions.category.NotFoundException;
+import com.ssafy.triptogether.global.exception.response.ErrorCode;
 import com.ssafy.triptogether.member.domain.Member;
 import com.ssafy.triptogether.member.repository.MemberRepository;
 
@@ -22,7 +25,10 @@ public class AuthServiceImpl implements AuthLoadService {
 	@Override
 	public Void pinVerify(Long memberId, PinVerifyRequest pinVerifyRequest) {
 		// Todo: 해당 member 가 존재하지 않는 예외 처리 작성
-
+		Member member = memberRepository.findById(memberId)
+			.orElseThrow(
+				() -> new NotFoundException("PinVerify", ErrorCode.UNDEFINED_MEMBER, memberId)
+			);
 		// Todo: inputPin 을 암호화한 후 member 의 PIN 과 비교
 
 		// Todo: 일치하지 않는다면 예외 처리
