92dd2be34caa7776b0207b6ed00a16faab8c23bc
hgene0929 <hgene0929@gmail.com>
2024-03-19 13:14:24 +0900

    S10P22A309-161 Feat: 프로필 조회 API 구현

--- BE/trip-together/src/main/java/com/ssafy/triptogether/global/data/response/StatusCode.java
+++ BE/trip-together/src/main/java/com/ssafy/triptogether/global/data/response/StatusCode.java
@@ -6,7 +6,8 @@ import lombok.Getter;
 public enum StatusCode {
     // SUCCESS CODE
     SUCCESS_PIN_VERIFY(200, "PIN 인증 완료"),
-    SUCCESS_PROFILE_UPDATE(200, "프로필 수정 성공");
+    SUCCESS_PROFILE_UPDATE(200, "프로필 수정 성공"),
+    SUCCESS_PROFILE_FIND(200, "프로필 조회 성공");
 
     private final int status;
     private final String message;
