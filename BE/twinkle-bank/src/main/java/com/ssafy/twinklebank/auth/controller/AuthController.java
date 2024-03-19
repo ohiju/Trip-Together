22cd5197b33840d1d47bb618b7c39ba6690aebd7
seomiii <seomiii1765@gmail.com>
2024-03-19 17:03:35 +0900

    S10P22A309-166 Feat: 토큰 발급 api 구현

--- BE/twinkle-bank/src/main/java/com/ssafy/twinklebank/auth/controller/AuthController.java
+++ BE/twinkle-bank/src/main/java/com/ssafy/twinklebank/auth/controller/AuthController.java
@@ -1,4 +1,62 @@
 package com.ssafy.twinklebank.auth.controller;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseCookie;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ssafy.twinklebank.auth.data.request.TokenRequest;
+import com.ssafy.twinklebank.auth.data.response.TokenResponse;
+import com.ssafy.twinklebank.auth.service.AuthServiceImpl;
+import com.ssafy.twinklebank.global.data.response.ApiResponse;
+import com.ssafy.twinklebank.global.data.response.StatusCode;
+
+import java.util.Map;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/member/v1/oauth")
+@RequiredArgsConstructor
 public class AuthController {
+	private final AuthServiceImpl authService;
+
+	@PostMapping("/token")
+	public ResponseEntity<ApiResponse<TokenResponse>> getToken(@RequestBody TokenRequest request) {
+
+		Map<String, String> tokenMap = authService.getToken(request);
+		String accessToken = tokenMap.get("access");
+		String refreshToken = tokenMap.get("refresh");
+
+		// refresh token은 헤더에 쿠키에 넣어준다
+		ResponseCookie cookie = ResponseCookie.from("refreshToken", refreshToken)
+			.maxAge(7 * 24 * 60 * 60)
+			.path("/")
+			.secure(true) // https를 통해서만 쿠키를 전송
+			.sameSite("None") //
+			.httpOnly(true)
+			.build();
+
+		// 쿠키를 담을 헤더 생성
+		HttpHeaders headers = new HttpHeaders();
+		headers.add(HttpHeaders.SET_COOKIE, cookie.toString());
+
+		// ApiResponse 객체 생성
+		ApiResponse<TokenResponse> apiResponse = ApiResponse.<TokenResponse>builder()
+			.status(StatusCode.SUCCESS_LOGIN.getStatus())
+			.message(StatusCode.SUCCESS_LOGIN.getMessage())
+			.data(new TokenResponse(accessToken))
+			.build();
+
+		// ResponseEntity에 헤더와 함께 ApiResponse 객체를 담아 반환
+		return ResponseEntity
+			.status(HttpStatus.OK)
+			.headers(headers)
+			.body(apiResponse);
+	}
+
 }
--- BE/twinkle-bank/src/main/java/com/ssafy/twinklebank/member/utils/MemberUtils.java
+++ BE/twinkle-bank/src/main/java/com/ssafy/twinklebank/member/utils/MemberUtils.java
@@ -1,4 +1,38 @@
 package com.ssafy.twinklebank.member.utils;
 
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.ssafy.twinklebank.global.exception.exceptions.WrongPasswordException;
+import com.ssafy.twinklebank.global.exception.exceptions.WrongUserNameOrPassWordException;
+import com.ssafy.twinklebank.member.domain.Member;
+import com.ssafy.twinklebank.member.repository.MemberRepository;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
 public class MemberUtils {
+	private final MemberRepository memberRepository;
+	private final PasswordEncoder passwordEncoder;
+
+	/**
+	 * username, password로 사용자를 가져오는 함수
+	 *
+	 * @param username
+	 * @param password
+	 * @return 인증된 유저
+	 */
+	public Member getMember(String username, String password) {
+
+		Member member = memberRepository.findMemberByUsername(username)
+			.orElseThrow(() -> new WrongUserNameOrPassWordException("MemberUtils"));
+
+		if (!passwordEncoder.matches(password, member.getPassword())) {
+			throw new WrongPasswordException("MemberUtils");
+		}
+		return member;
+	}
 }
