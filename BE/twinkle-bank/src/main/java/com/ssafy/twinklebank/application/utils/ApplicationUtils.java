70cbbb67f6f2a4df1fb7df9ea975e281c609a018
seomiii <seomiii1765@gmail.com>
2024-03-20 16:41:44 +0900

    S10P22A309-202 Feat: application utils error 수정

--- BE/twinkle-bank/src/main/java/com/ssafy/twinklebank/application/utils/ApplicationUtils.java
+++ BE/twinkle-bank/src/main/java/com/ssafy/twinklebank/application/utils/ApplicationUtils.java
@@ -4,7 +4,8 @@ import org.springframework.stereotype.Service;
 
 import com.ssafy.twinklebank.application.domain.Application;
 import com.ssafy.twinklebank.application.repository.ApplicationRepository;
-import com.ssafy.twinklebank.global.exception.exceptions.ApplicationNotFoundException;
+import com.ssafy.twinklebank.global.exception.exceptions.category.NotFoundException;
+import com.ssafy.twinklebank.global.exception.response.ErrorCode;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -16,8 +17,7 @@ public class ApplicationUtils {
 	private final ApplicationRepository applicationRepository;
 
 	public Application getApplication(String clientId) {
-		Application application = applicationRepository.findByClientId(clientId)
-			.orElseThrow(() -> new ApplicationNotFoundException("ApplicationUtils"));
-		return application;
+		return applicationRepository.findByClientId(clientId)
+			.orElseThrow(() -> new NotFoundException("ApplicationUtils : ",ErrorCode.APPLICATION_NOT_FOUND));
 	}
 }
