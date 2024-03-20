a74f6679ee6c6b2f1e2583b5010986b417ce283e
hgene0929 <hgene0929@gmail.com>
2024-03-20 10:41:30 +0900

    S10P22A309-255 Test: 은행 사용자 정보 조회 API 서비스 단위 테스트

---
+++ BE/twinkle-bank/src/test/java/com/ssafy/twinklebank/member/service/MemberServiceImplTest.java
@@ -0,0 +1,39 @@
+package com.ssafy.twinklebank.member.service;
+
+import com.ssafy.twinklebank.global.exception.exceptions.category.NotFoundException;
+import com.ssafy.twinklebank.member.repository.MemberRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static com.ssafy.twinklebank.global.exception.response.ErrorCode.UNDEFINED_MEMBER;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@ExtendWith(MockitoExtension.class)
+class MemberServiceImplTest {
+
+    @InjectMocks
+    MemberServiceImpl memberService;
+    @Mock
+    MemberRepository memberRepository;
+
+    @Test
+    @DisplayName("[FindAuthInfo][Error] undefined member")
+    void authInfoNotFoundException() {
+        // given
+        String undefinedUuid = "undefined";
+
+        // when
+        NotFoundException exception
+                = assertThrows(NotFoundException.class, () -> memberService.findAuthInfo(undefinedUuid));
+
+        // then
+        assertThat(exception.getMessageKey()).isEqualTo("error.NotFound.AuthInfoFind");
+        assertThat(exception.getErrorCode()).isEqualTo(UNDEFINED_MEMBER);
+        assertThat(exception.getParams()).contains(undefinedUuid);
+    }
+}
