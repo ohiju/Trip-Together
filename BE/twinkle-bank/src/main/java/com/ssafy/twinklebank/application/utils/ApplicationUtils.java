67ad5aa4e3ec2e00ad086d760d84694ed75993e7
seomiii <seomiii1765@gmail.com>
2024-03-20 16:33:16 +0900

    S10P22A309-202 Feat: client id로 application 가져오는 로직 구현

--- BE/twinkle-bank/src/main/java/com/ssafy/twinklebank/application/utils/ApplicationUtils.java
+++ BE/twinkle-bank/src/main/java/com/ssafy/twinklebank/application/utils/ApplicationUtils.java
@@ -1,4 +1,23 @@
 package com.ssafy.twinklebank.application.utils;
 
+import org.springframework.stereotype.Service;
+
+import com.ssafy.twinklebank.application.domain.Application;
+import com.ssafy.twinklebank.application.repository.ApplicationRepository;
+import com.ssafy.twinklebank.global.exception.exceptions.ApplicationNotFoundException;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
 public class ApplicationUtils {
+	private final ApplicationRepository applicationRepository;
+
+	public Application getApplication(String clientId) {
+		Application application = applicationRepository.findByClientId(clientId)
+			.orElseThrow(() -> new ApplicationNotFoundException("ApplicationUtils"));
+		return application;
+	}
 }
