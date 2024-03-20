0266b2135189f9ffc192e69345b26e66019f7ee6
hgene0929 <hgene0929@gmail.com>
2024-03-20 10:40:50 +0900

    S10P22A309-255 Fix: 글로벌 예외 최상위 타입명 수정

--- BE/twinkle-bank/src/main/java/com/ssafy/twinklebank/global/exception/exceptions/category/BadRequestException.java
+++ BE/twinkle-bank/src/main/java/com/ssafy/twinklebank/global/exception/exceptions/category/BadRequestException.java
@@ -7,7 +7,7 @@ import com.ssafy.twinklebank.global.exception.response.ErrorCode;
  * 잘못된 요청.
  * 문법등의 오류로 서버가 요청사항을 이해하지 못함
  */
-public class BadRequestException extends TripRuntimeException {
+public class BadRequestException extends BankRuntimeException {
 	protected static final String MESSAGE_KEY = "error.BadRequest";
 	public BadRequestException(String detailMessageKey, ErrorCode errorCode, Object... params) {
 		super(MESSAGE_KEY + "." + detailMessageKey, errorCode, params);
