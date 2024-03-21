72a7aef6ca8a2162366ea13bd6d48dbd8ea2488a
seomiii <seomiii1765@gmail.com>
2024-03-21 13:22:51 +0900

    S10P22A309-202 Refactor: findById 삭제

--- BE/twinkle-bank/src/main/java/com/ssafy/twinklebank/member/repository/MemberRepository.java
+++ BE/twinkle-bank/src/main/java/com/ssafy/twinklebank/member/repository/MemberRepository.java
@@ -8,5 +8,4 @@ import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long>, MemberRepositoryCustom {
 	Optional<Member> findMemberByUsername(String username);
-	Optional<Member> findById(Long id);
 }
