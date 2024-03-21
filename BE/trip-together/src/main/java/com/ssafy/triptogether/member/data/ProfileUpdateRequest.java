dbf26fd60cb4ca07576ab8604ad9028f556bc345
hgene0929 <hgene0929@gmail.com>
2024-03-21 16:40:42 +0900

    S10P22A309-167 Chore: 프로필 수정 API 서비스 유효성 검증 로직 심화

--- BE/trip-together/src/main/java/com/ssafy/triptogether/member/data/ProfileUpdateRequest.java
+++ BE/trip-together/src/main/java/com/ssafy/triptogether/member/data/ProfileUpdateRequest.java
@@ -1,11 +1,12 @@
 package com.ssafy.triptogether.member.data;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotBlank;
 
 public record ProfileUpdateRequest(
         @JsonProperty("image_url") String imageUrl,
-        @NotNull String nickname,
+        @NotBlank(message = "변경할 닉네임을 입력해주세요.")
+        String nickname,
         String description
 ) {
 }
