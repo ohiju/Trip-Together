9b82381e3ac74a624247548b0eff35750ad76f88
황진하 <hjh70902000@naver.com>
2024-03-21 13:16:47 +0900

    S10P22A309-242 Feat: 연동 계좌 등록 v1

--- BE/twinkle-bank/src/main/java/com/ssafy/twinklebank/account/repository/AccountRepository.java
+++ BE/twinkle-bank/src/main/java/com/ssafy/twinklebank/account/repository/AccountRepository.java
@@ -7,7 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface AccountRepository extends JpaRepository<Account, Long>, AccountRepositoryCustom {
-    void addLinkedAccount(long userId, String accountUUUID);
-
     Optional<Account> findAccountByUuid(String uuid);
 }
