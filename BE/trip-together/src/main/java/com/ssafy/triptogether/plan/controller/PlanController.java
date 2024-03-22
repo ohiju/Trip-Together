80bb2a73f3645e89f1ec9277894d89c3f92e063d
sirlyun <sirlyun23@gmail.com>
2024-03-22 17:24:40 +0900

    S10P22A309-241 Feat: Controller 인증 객체 수신 추가

--- BE/trip-together/src/main/java/com/ssafy/triptogether/plan/controller/PlanController.java
+++ BE/trip-together/src/main/java/com/ssafy/triptogether/plan/controller/PlanController.java
@@ -2,6 +2,7 @@ package com.ssafy.triptogether.plan.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.ssafy.triptogether.auth.utils.SecurityMember;
 import com.ssafy.triptogether.global.data.response.ApiResponse;
 import com.ssafy.triptogether.global.data.response.StatusCode;
 import com.ssafy.triptogether.plan.data.request.PlansSaveRequest;
@@ -26,9 +28,11 @@ public class PlanController {
 
 	@PostMapping("/plans")
 	public ResponseEntity<ApiResponse<Void>> plansSave(
+		@AuthenticationPrincipal SecurityMember securityMember,
 		@RequestBody @Valid PlansSaveRequest plansSaveRequest
 	) {
-		planSaveService.plansSave(1L, plansSaveRequest);
+		long memberId = securityMember.getId();
+		planSaveService.plansSave(memberId, plansSaveRequest);
 
 		return ApiResponse.emptyResponse(
 			HttpStatus.CREATED, StatusCode.SUCCESS_PLANS_SAVE
@@ -37,9 +41,11 @@ public class PlanController {
 
 	@DeleteMapping("/plans/{plan_id}")
 	public ResponseEntity<ApiResponse<Void>> planDelete(
+		@AuthenticationPrincipal SecurityMember securityMember,
 		@PathVariable("plan_id") Long planId
 	) {
-		planSaveService.planDelete(1L, planId);
+		long memberId = securityMember.getId();
+		planSaveService.planDelete(memberId, planId);
 
 		return ApiResponse.emptyResponse(
 			HttpStatus.NO_CONTENT, StatusCode.SUCCESS_PLAN_DELETE
