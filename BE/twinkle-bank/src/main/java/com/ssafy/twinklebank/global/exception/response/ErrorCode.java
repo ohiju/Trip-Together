9a44b2e6d5434dc5f710088bb66ad6a6a3481c95
seomiii <seomiii1765@gmail.com>
2024-03-22 14:02:15 +0900

    S10P22A309-160 Feat : 인증발급 api 구현

--- BE/twinkle-bank/src/main/java/com/ssafy/twinklebank/global/exception/response/ErrorCode.java
+++ BE/twinkle-bank/src/main/java/com/ssafy/twinklebank/global/exception/response/ErrorCode.java
@@ -13,9 +13,13 @@ public enum ErrorCode {
 
 	// APPLICATION
 	APPLICATION_NOT_FOUND(404, "등록된 APPLICATION이 아닙니다."),
+	WRONG_CLIENT_ID(401, "잘못된 CLIENT ID입니다."),
 
 	// COOKIE
 	COOKIE_NOT_FOUND(404, "해당하는 쿠키가 없습니다."),
+
+	// CODE
+	CODE_NOT_FOUND(404, "인증 코드가 없습니다."),
 
 	REFRESH_NOT_FOUND(404, "refresh token이 비어있습니다."),
 	UNAUTHORIZED_REFRESH(401,"유효하지 않은 refresh token입니다."),
