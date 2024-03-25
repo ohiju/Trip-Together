49ce840e0e8403c33be6a9a226667493eac46a7a
hgene0929 <hgene0929@gmail.com>
2024-03-25 17:45:39 +0900

    S10P22A309-324 Chore: MongoDB 로직 추가

--- BE/trip-together/src/main/java/com/ssafy/triptogether/plan/controller/PlanController.java
+++ BE/trip-together/src/main/java/com/ssafy/triptogether/plan/controller/PlanController.java
@@ -25,11 +25,11 @@ public class PlanController {
 
     @PostMapping
     public ResponseEntity<ApiResponse<Void>> plansSave(
-            @AuthenticationPrincipal SecurityMember securityMember,
+//            @AuthenticationPrincipal SecurityMember securityMember,
             @RequestBody @Valid PlansSaveRequest plansSaveRequest
     ) {
-        long memberId = securityMember.getId();
-        planSaveService.plansSave(memberId, plansSaveRequest);
+//        long memberId = securityMember.getId();
+        planSaveService.plansSave(1L, plansSaveRequest);
 
         return ApiResponse.emptyResponse(CREATED, SUCCESS_PLANS_SAVE);
     }
