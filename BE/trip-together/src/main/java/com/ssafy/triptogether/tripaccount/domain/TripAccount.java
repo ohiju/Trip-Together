53ed97aaf43324d6852a3bdb554c222fab9a2ccf
sirlyun <sirlyun23@gmail.com>
2024-03-25 12:02:45 +0900

    S10P22A309-245 Refactor: balance 컬럼 조건 변경

--- BE/trip-together/src/main/java/com/ssafy/triptogether/tripaccount/domain/TripAccount.java
+++ BE/trip-together/src/main/java/com/ssafy/triptogether/tripaccount/domain/TripAccount.java
@@ -4,7 +4,9 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.ssafy.triptogether.global.domain.BaseEntity;
 import com.ssafy.triptogether.member.domain.Member;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -31,7 +33,8 @@ public class TripAccount extends BaseEntity {
     @JoinColumn(name = "currency_id")
     private Currency currency;
 
-    @NotBlank
+    @NotNull
+    @Min(0)
     @Column(name = "balance")
     private Double balance;
 
