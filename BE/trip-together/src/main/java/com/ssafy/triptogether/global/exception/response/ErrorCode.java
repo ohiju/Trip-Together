d00b19e1134453ccfc873d43f445950f8b45f37d
sirlyun <sirlyun23@gmail.com>
2024-03-27 21:04:47 +0900

    S10P22A309-329 Fix: PIN 인증 로직 작성

--- BE/trip-together/src/main/java/com/ssafy/triptogether/global/exception/response/ErrorCode.java
+++ BE/trip-together/src/main/java/com/ssafy/triptogether/global/exception/response/ErrorCode.java
@@ -33,7 +33,7 @@ public enum ErrorCode {
     DAILY_PLAN_NOT_FOUND(404, "일자별 여행 계획을 찾을 수 없습니다."),
     // REVIEW
     REVIEW_NOT_FOUND(404, "여행지를 찾을 수 없습니다"),
-
+    // AUTH
 	UNDEFINED_ACCESS_TOKEN(400, "ACCESS TOKEN 존재하지 않습니다."),
 	UNDEFINED_REFRESH_TOKEN(400, "REFRESH TOKEN 존재하지 않습니다."),
     REFRESH_NOT_FOUND(404, "REFRESH 토큰을 찾을 수 없습니다."),
