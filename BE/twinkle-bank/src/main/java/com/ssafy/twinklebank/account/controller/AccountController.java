cb377e0bc4eae8d45f63273d9ab60a0bb980d729
황진하 <hjh70902000@naver.com>
2024-03-27 20:19:49 +0900

    S10P22A309-232 Fix: SecurityMember로부터 id를 가져오도록 변경

--- BE/twinkle-bank/src/main/java/com/ssafy/twinklebank/account/controller/AccountController.java
+++ BE/twinkle-bank/src/main/java/com/ssafy/twinklebank/account/controller/AccountController.java
@@ -41,12 +41,11 @@ public class AccountController {
 
     @GetMapping
     public ResponseEntity<ApiResponse<getUserAccountsResponse>> getUserAccountList(
-        // @AuthenticationPrincipal SecurityMember securityMember,
+        @AuthenticationPrincipal SecurityMember securityMember,
         @RequestParam("client_id") String clientId
     ) {
-        // long userId = securityMember.getId();
-        long userId = 3L;
-        List<AccountResponse> accountResponseList = accountLoadService.getAccounts(clientId, userId);
+        long memberId = securityMember.getId();
+        List<AccountResponse> accountResponseList = accountLoadService.getAccounts(clientId, memberId);
 
         return ApiResponse.toResponseEntity(OK, SUCCESS_GET_ACCOUNT_LIST,
             getUserAccountsResponse.builder().accounts(accountResponseList).build());
