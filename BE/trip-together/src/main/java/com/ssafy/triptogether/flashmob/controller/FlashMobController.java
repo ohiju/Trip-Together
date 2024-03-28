09833df4eee8ae4c7eca5be4ff461d47b8bdac8e
hgene0929 <hgene0929@gmail.com>
2024-03-28 09:29:33 +0900

    S10P22A309-309 Feat: 번개 채팅 참가신청 API 개발

--- BE/trip-together/src/main/java/com/ssafy/triptogether/flashmob/controller/FlashMobController.java
+++ BE/trip-together/src/main/java/com/ssafy/triptogether/flashmob/controller/FlashMobController.java
@@ -1,4 +1,33 @@
 package com.ssafy.triptogether.flashmob.controller;
 
+import com.ssafy.triptogether.auth.utils.SecurityMember;
+import com.ssafy.triptogether.flashmob.service.FlashMobSaveService;
+import com.ssafy.triptogether.global.data.response.ApiResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static com.ssafy.triptogether.global.data.response.StatusCode.SUCCESS_FLASHMOB_REQUEST;
+import static org.springframework.http.HttpStatus.CREATED;
+
+@RestController
+@RequestMapping("/flashmob/v1/flashmobs")
+@RequiredArgsConstructor
 public class FlashMobController {
+
+    private final FlashMobSaveService flashMobSaveService;
+
+    @GetMapping("/{flashmob_id}")
+    public ResponseEntity<ApiResponse<Void>> sendAttendanceRequest(
+        @PathVariable("flashmob_id") long flashmobId,
+        @AuthenticationPrincipal SecurityMember securityMember
+    ) {
+        long memberId = securityMember.getId();
+        flashMobSaveService.sendAttendanceRequest(flashmobId, memberId);
+        return ApiResponse.emptyResponse(CREATED, SUCCESS_FLASHMOB_REQUEST);
+    }
 }
