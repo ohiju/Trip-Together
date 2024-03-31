424358ab9f20bc7969b7bd321a7f40e13cacea77
hgene0929 <hgene0929@gmail.com>
2024-03-31 15:39:09 +0900

    S10P22A309-339 Chore: 여행계획 상세조회 API 수정

--- BE/trip-together/src/main/java/com/ssafy/triptogether/plan/data/response/DailyPlanResponse.java
+++ BE/trip-together/src/main/java/com/ssafy/triptogether/plan/data/response/DailyPlanResponse.java
@@ -1,10 +1,13 @@
 package com.ssafy.triptogether.plan.data.response;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.ssafy.triptogether.attraction.domain.Nation;
 
 import java.time.LocalDate;
 
 public record DailyPlanResponse(
+    @JsonProperty("plan_id") Long planId,
+    Nation nation,
     @JsonProperty("start_region") String startRegion,
     @JsonProperty("start_at") LocalDate startAt,
     @JsonProperty("end_at") LocalDate endAt,
