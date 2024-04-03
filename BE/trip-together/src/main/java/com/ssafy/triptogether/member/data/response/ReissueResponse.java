14c0909008232bc90e7de17fed59bef1e80ddc50
seomiii <seomiii1765@gmail.com>
2024-04-03 16:00:02 +0900

    Hotfix: refresh token 변수명 변경
    Former-commit-id: 788893ec3f5e714554c85d69d53d9a2dfbcde224

--- BE/trip-together/src/main/java/com/ssafy/triptogether/member/data/response/ReissueResponse.java
+++ BE/trip-together/src/main/java/com/ssafy/triptogether/member/data/response/ReissueResponse.java
@@ -8,7 +8,7 @@ import lombok.Builder;
 
 @Builder
 public record ReissueResponse(
-	@NotBlank @JsonProperty("access")
+	@NotBlank @JsonProperty("access_token")
 	String access,
 	@NotNull @JsonProperty("expires_in")
 	Long expiresIn,
